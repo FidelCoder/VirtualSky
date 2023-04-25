73de8c2ab1d6a424bc35b04e846621b39e6c5011
Griffins-sys254 <griffinesonyango@gmail.com>
2023-04-25 20:04:40 +0300

    api change

--- frontend/src/components/Auth/AuthForm.jsx
+++ frontend/src/components/Auth/AuthForm.jsx
@@ -35,7 +35,7 @@ const AuthForm = () => {
   
     // Save interests in the database
     try {
-      await axios.post(`https://node-express-vercel-rose.vercel.app/api/users/${authCtx.userId}/interests`, {
+      await axios.post(`https://virtual-sky-servers-dkix.vercel.app/api/users/${authCtx.userId}/interests`, {
         interests: selectedInterests,
       });
   
@@ -75,7 +75,7 @@ const AuthForm = () => {
   
     try {
       setIsLoading(true);
-      const res = await fetch(isLogin ? 'https://node-express-vercel-rose.vercel.app/login' : 'https://node-express-vercel-rose.vercel.app/signup', {
+      const res = await fetch(isLogin ? 'https://virtual-sky-servers-dkix.vercel.app/login' : 'https://virtual-sky-servers-dkix.vercel.app/signup', {
         method: 'POST',
         body: JSON.stringify(userPayload),
         headers: {
--- frontend/src/pages/ObjectInfoModal.jsx
+++ frontend/src/pages/ObjectInfoModal.jsx
@@ -82,7 +82,7 @@ const InterestSelection = ({ onInterestSelected }) => {
     onInterestSelected(selectedInterests);
 
     try {
-      await axios.post('https://node-express-vercel-rose.vercel.app/api/interests', {
+      await axios.post('https://virtual-sky-servers-dkix.vercel.app/api/interests', {
         interests: selectedInterests,
       });
     } catch (error) {
