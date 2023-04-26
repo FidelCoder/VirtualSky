1383eb47165c95de7ebedd9bc8edba91a45ec279
Griffins-sys254 <griffinesonyango@gmail.com>
2023-04-26 16:25:44 +0300

    client-side changes

--- frontend/src/components/Auth/AuthForm.jsx
+++ frontend/src/components/Auth/AuthForm.jsx
@@ -35,7 +35,7 @@ const AuthForm = () => {
   
     // Save interests in the database
     try {
-      await axios.post(`https://virtual-sky-servers-dkix.vercel.app/api/users/${authCtx.userId}/interests`, {
+      await axios.post(`https://virtual-sky-servers-dkix-a39qz91nk-griffins-sys254.vercel.app/api/users/${authCtx.userId}/interests`, {
         interests: selectedInterests,
       });
   
@@ -141,7 +141,7 @@ const AuthForm = () => {
   
     try {
       setIsLoading(true);
-      const res = await axios.post(isLogin ? 'https://virtual-sky-servers-dkix.vercel.app/login' : 'https://virtual-sky-servers-dkix.vercel.app/signup', userPayload);
+      const res = await axios.post(isLogin ? 'https://virtual-sky-servers-dkix-a39qz91nk-griffins-sys254.vercel.app//login' : 'https://virtual-sky-servers-dkix-a39qz91nk-griffins-sys254.vercel.app/signup', userPayload);
   
       const data = res.data;
       console.log('Received data:', data);
